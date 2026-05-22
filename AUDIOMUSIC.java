a904606be831bb703c783d3779b5a0e3383722e8
DeyvisValencia18 <fdeyvis092@gmail.com>
2026-05-21 19:21:33 -0500

    Update AUDIOMUSIC.java
    //ES LA BOLETA

--- AUDIOMUSIC.java
+++ AUDIOMUSIC.java
@@ -1,14 +1,22 @@
+import java.time.Year;
 import java.util.Scanner;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 public class AUDIOMUSIC {
     public static void  main    (String[]args){
         Scanner sc=new Scanner(System.in);
         String nombre, apellido,  correo, telefono;
         String metodoPago="";
+        String ubiDelRecojo="";
+        String respuestaLUGAR="";
         int opcion,documento;
-        double monto_TOTAL,pago,vuelto,igv;
+        double monto_TOTAL=40,pago,vuelto,igv;
         boolean datosCorrectos;
-        System.out.println("====CARRITO DE COMPRAS====");
-        System.out.println("==DATOS PERSONALES==");
+        System.out.println("=====================================");
+        System.out.println("      CARRITO DE COMPRAS-AUDIOMUSIC");
+        System.out.println("           DATOS PERSONALES");
+        System.out.println("=====================================");
         do {
             datosCorrectos = true;
             System.out.println("Ingrese sus nombres");
@@ -17,11 +25,13 @@ public class AUDIOMUSIC {
             apellido = sc.nextLine();
             System.out.println("Ingrese su documento de identidad");
             documento= sc.nextInt();
+            sc.nextLine();
             System.out.println("Ingrese su correo electronico");
             correo = sc.nextLine();
+
             System.out.println("Ingrese su telefono");
             telefono = sc.nextLine();
-            if (nombre.equals("")||apellido.equals("")||correo.equals("")||telefono.equals("")){
+            if (nombre.equals("")||apellido.equals("")){
                 System.out.println("UNO DE LOS DATOS ESTA VACIO! INTENTALO DE NUEVO");
                 datosCorrectos=false;
             }
@@ -33,30 +43,77 @@ public class AUDIOMUSIC {
         System.out.println("===SELCCIONE EL METODO DE PAGO (marque un numero)===");
         System.out.println("1.- TARJETA DE CREDITO O DEBITO");
         System.out.println("2.- PAGO POR DEPOSITO");
-        System.out.println("3.- YAPE PLIN ");
+        System.out.println("3.- EFECTIVO");
         opcion= sc.nextInt();
         switch (opcion){
             case 1:
                 metodoPago="TARJETA";
+                validaPagoTarjeta( sc, monto_TOTAL);
                 break;
             case 2:
                 metodoPago="PAGO POR DEPOSITO";
                 break;
             case 3:
-                metodoPago="YAPE O PLIN";
+                metodoPago="EFECTIVO";
                 break;
             default:
                 System.out.println("OPCION INCORRECTO INTENTELO DE NUEVO");
         }
-        System.out.println("PROCESANDO PAGO......");
+        System.out.println("El monto total ah pagar es:   " + monto_TOTAL);
         System.out.println("INGRESA MONTO AH PAGAR");
         pago= sc.nextDouble();
+        System.out.println("PROCESANDO PAGO......");
+        System.out.println("TENEMOS 3 TIENDAS FISICAS, DONDE SE LE PUEDE ENTREGAR EL INSTRUMENTO O ENVIOS AH NIVEL NACIONAL (marque un numero)");
+        System.out.println("1.-   JULIACA");
+        System.out.println("2.-   PUNO");
+        System.out.println("3.-   AREQUIPA");
+        System.out.println("4.-   ENVIO POR UNA AGENCIA");
+        int opcionENTREGA= sc.nextInt();
+        switch (opcionENTREGA){
+            case    1:
+                ubiDelRecojo="JULIACA";
+                break;
+            case    2:
+                ubiDelRecojo="PUNO";
+                break;
+            case    3:
+                ubiDelRecojo="AREQUIPA";
+                break;
+            case    4:
+                System.out.println("CUAL ES LA AGENCIA DE SU PREFERENCIA:");
+                System.out.println("1.-   SHALOM");
+                System.out.println("2.-   FLORES");
+                System.out.println("3.-   MARVISUR");
+                int opcion77= sc.nextInt();
+                switch (opcion77){
+                    case 1:
+                        sc.nextLine();
+                        System.out.println("ESCRIBA SU LUGAR DE DESTINO");
+                        respuestaLUGAR= sc.nextLine();
+                        break;
+                    case 2:
+                        sc.nextLine();
+                        System.out.println("ESCRIBA SU LUGAR DE DESTINO");
+                        respuestaLUGAR= sc.nextLine();
+                        break;
+                    case 3:
+                        sc.nextLine();
+                        System.out.println("ESCRIBA SU LUGAR DE DESTINO");
+                        respuestaLUGAR= sc.nextLine();
+                        break;
+                    default:
+                        System.out.println("OPCION NO VALIDA!!");
+                }
+                break;
+            default:
+                System.out.println("OPCION NO VALIDA!!");
+        }
         if (pago>=monto_TOTAL){
             vuelto=calcularVuelto(pago,monto_TOTAL);
             igv=calcularIGV(monto_TOTAL);
             System.out.println("=====================================");
             System.out.println("     BOLETA DE VENTA ELECTRONICO     ");
-            System.out.println("            AUDIOMUSIC     ");
+            System.out.println("            AUDIOMUSIC               ");
             System.out.println("=====================================");
             System.out.println("Nombres:       "+nombre);
             System.out.println("Apellidos:       "+apellido);
@@ -64,8 +121,10 @@ public class AUDIOMUSIC {
             System.out.println("Correo electronico:       "+correo);
             System.out.println("Telefono:       "+telefono);
             System.out.println("Metodo de pago:       "+metodoPago);
+            System.out.println("Entrega en la tienda de:"    +ubiDelRecojo );
+            System.out.println("Destino de envio:"          +respuestaLUGAR);
 
-            System.out.println("----------------------------------------");
+            System.out.println("---------------------------------------");
 
 
             System.out.println("IGV:"      +igv);
@@ -87,6 +146,58 @@ public class AUDIOMUSIC {
         return pago-monto_TOTAL;
     }
     public static double calcularIGV(double monto_TOTAL){
-        return monto_TOTAL*1.18;
+        return monto_TOTAL*0.18;
+    }
+    public static void validaPagoTarjeta(Scanner sc,double monto_TOTAL) {
+        String nroTarjeta, fechaVnecimiento, cvv;
+        boolean paAprovado = false;
+        System.out.println("El monto total ah pagar es:   " + monto_TOTAL);
+        do {
+            System.out.println("Ingrese los 16 digitos de la tarjeta");
+            nroTarjeta = sc.nextLine();
+            sc.nextLine();
+            System.out.println("Ingrese la fecha de caducidad (MM/AA)");
+            fechaVnecimiento = sc.nextLine();
+            System.out.println("Ingrese el codgio de seguridad");
+            cvv = sc.nextLine();
+            boolean tarjetaOK=(nroTarjeta.length()==16);
+            boolean cvvOk=(cvv.length()==3);
+            boolean fechaEstructuraOK=(fechaVnecimiento.length()==5&&fechaVnecimiento.contains("/"));
+            boolean fechaNovencida=false;
+            if (fechaEstructuraOK){
+                try {
+                    DateTimeFormatter Formateador=DateTimeFormatter.ofPattern("MM/yy");
+                    YearMonth fechaTarjeta=YearMonth.parse(fechaVnecimiento,Formateador);
+                    YearMonth fechaActual= YearMonth.now();
+                    if (fechaTarjeta.isAfter(fechaActual)||fechaTarjeta.equals(fechaActual)){
+                        fechaNovencida=true;
+                    }
+                }
+                catch (DateTimeParseException error){
+                    fechaEstructuraOK=false;
+                }
+            }
+            if (tarjetaOK&&fechaEstructuraOK&&fechaNovencida){
+                System.out.println(" TARJETA VALIDA");
+                paAprovado=true;
+            }
+            else {
+                System.out.println("Operacion cancelada....");
+            }
+            if (!tarjetaOK){
+                System.out.println("El numero de tarjeta debe contener exactamente 16 digitos");
+            }
+            if(!fechaEstructuraOK){
+                System.out.println("Fromato de fecha incorrecta");
+            }
+            if (fechaEstructuraOK&&!fechaNovencida){
+                System.out.println("¡TARJETA CADUCADA!");
+            }
+            if (!cvvOk){
+                System.out.println("Intente nuevamente el pago...");
+            }
+        }
+        while (!paAprovado);
     }
 }
+
